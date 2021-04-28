fc12988e7108c6b4ab5eaf07ed709cdd46ceb114
Kevin Läufer <laeufer@cs.berkeley.edu>
2021-04-28 11:45:00 -0700

    test: failing witness parser test

---
+++ test/maltese/mc/Btor2WitnessParserTest.scala
@@ -0,0 +1,22 @@
+package maltese.mc
+
+import org.scalatest.flatspec.AnyFlatSpec
+
+class Btor2WitnessParserTest extends AnyFlatSpec {
+  behavior of "Btor2WitnessParser"
+
+  val noStateWitness =
+    """sat
+      |b0
+      |@0
+      |0 0 reset@0
+      |1 11111011 a@0
+      |2 00000101 b@0
+      |.
+      |
+      |""".stripMargin
+
+  it should "parse a witness without state" ignore { // TODO: fix witness parser
+    Btor2WitnessParser.read(noStateWitness.split("\n"))
+  }
+}
