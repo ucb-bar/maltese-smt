8ab3edb852b810794487a0229b53a507e58770d9
Kevin Läufer <laeufer@cs.berkeley.edu>
2021-09-08 15:29:02 -0700

    update expression converter to latest firrtl changes

--- src/firrtl/backends/experimental/smt/ExpressionConverter.scala
+++ src/firrtl/backends/experimental/smt/ExpressionConverter.scala
@@ -11,9 +11,7 @@ import maltese.{smt => m}
 
 /** converts between firrtl's internal SMT expr library and the maltese expression library */
 object ExpressionConverter {
-  private case class Context() extends TranslationContext
   def toMaltese(e: ir.Expression, width: Int, allowNarrow: Boolean): m.BVExpr = {
-    implicit val ctx: TranslationContext = Context()
     val firSmt = FirrtlExpressionSemantics.toSMT(e, width, allowNarrow)
     toMaltese(firSmt)
   }
@@ -25,13 +23,17 @@ object ExpressionConverter {
     val inputs = sys.inputs.map(toMaltese)
     val states = sys.states.map(toMaltese)
     val signals = sys.signals.map { s =>
-      val lbl = if (sys.outputs.contains(s.name)) { mc.IsOutput }
-      else if (sys.assumes.contains(s.name)) { mc.IsConstraint }
-      else if (sys.asserts.contains(s.name)) { mc.IsBad }
-      else if (sys.fair.contains(s.name)) { mc.IsFair }
-      else { mc.IsNode }
+      val lbl = s.lbl match {
+        case IsFair       => mc.IsFair
+        case IsOutput     => mc.IsOutput
+        case IsNext       => mc.IsNext
+        case IsInit       => mc.IsInit
+        case IsNode       => mc.IsNode
+        case IsConstraint => mc.IsConstraint
+        case IsBad        => mc.IsBad
+      }
       val eMaltese = toMaltese(s.e)
-      val expr = if (lbl == mc.IsBad) m.BVNot(eMaltese) else eMaltese
+      val expr = if (lbl == mc.IsBad) m.BVNot(eMaltese.asInstanceOf[m.BVExpr]) else eMaltese
       mc.Signal(s.name, expr, lbl)
     }
     mc.TransitionSystem(sys.name, inputs.toList, states.toList, signals.toList)
@@ -60,7 +62,7 @@ object ExpressionConverter {
     case r: BVReduceOr  => toMaltese(Expander.expand(r))
     case r: BVReduceAnd => toMaltese(Expander.expand(r))
     case r: BVReduceXor => toMaltese(Expander.expand(r))
-    case BVImplies(a, b)                   => toMaltese(implies(a, b))
+    case BVImplies(a, b)                   => m.BVImplies(toMaltese(a), toMaltese(b))
     case BVEqual(a, b)                     => m.BVEqual(toMaltese(a), toMaltese(b))
     case BVComparison(op, a, b, signed)    => m.BVComparison(toMalteseCmp(op), toMaltese(a), toMaltese(b), signed)
     case BVOp(op, a, b)                    => m.BVOp(toMalteseOp(op), toMaltese(a), toMaltese(b))
@@ -68,11 +70,10 @@ object ExpressionConverter {
     case ArrayRead(array, index)           => m.ArrayRead(toMaltese(array), toMaltese(index))
     case BVIte(cond, tru, fals)            => m.BVIte(toMaltese(cond), toMaltese(tru), toMaltese(fals))
     case ArrayEqual(a, b)                  => m.ArrayEqual(toMaltese(a), toMaltese(b))
-    case BVRawExpr(serialized, _)          => throw new NotImplementedError(s"Unsupported RawExpr: $serialized")
     case BVFunctionCall(name, args, width) => m.BVFunctionCall(name, args.map(toMaltese), width)
+    case BVAnd(terms)                      => m.BVAnd(terms.map(toMaltese))
+    case BVOr(terms)                       => m.BVOr(terms.map(toMaltese))
   }
-
-  private def implies(a: BVExpr, b: BVExpr): BVExpr = BVOp(Op.Or, BVNot(a), b)
 
   def toMaltese(sym: BVSymbol): m.BVSymbol = m.BVSymbol(sym.name, sym.width)
 
@@ -87,8 +88,6 @@ object ExpressionConverter {
   }
 
   private def toMalteseOp(op: Op.Value): m.Op.Value = op match {
-    case Op.And                  => m.Op.And
-    case Op.Or                   => m.Op.Or
     case Op.Xor                  => m.Op.Xor
     case Op.ShiftLeft            => m.Op.ShiftLeft
     case Op.ArithmeticShiftRight => m.Op.ArithmeticShiftRight
@@ -108,7 +107,10 @@ object ExpressionConverter {
     case ArrayStore(array, index, data)           => m.ArrayStore(toMaltese(array), toMaltese(index), toMaltese(data))
     case ArrayIte(cond, tru, fals)                => m.ArrayIte(toMaltese(cond), toMaltese(tru), toMaltese(fals))
     case ArrayConstant(e, indexWidth)             => m.ArrayConstant(toMaltese(e), indexWidth)
-    case ArrayRawExpr(serialized, _, _)           => throw new NotImplementedError(s"Unsupported RawExpr: $serialized")
   }
 
+  def toMaltese(a: SMTFunctionArg): m.SMTFunctionArg = a match {
+    case expr: SMTExpr => toMaltese(expr)
+    case UTSymbol(name, tpe) => m.UTSymbol(name, tpe)
+  }
 }
